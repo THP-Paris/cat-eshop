08a91b946590a57fedb7b74e53ab2cc46681fe12
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-06-07 16:36:01 +0200

    wip

--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,10 @@
 Rails.application.routes.draw do
-  
-  devise_for :users
-  get 'item/index'
-  get 'item/new'
-  get 'item/create'
-  get 'item/update'
   root to: 'item#index'
+
+  devise_for :users
+
+  resources :item
+  
   
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
