da3244f5089e7f6ae18fb8b14296d3e9ae33befa
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-06-09 12:17:51 +0200

    order controller

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,5 +1,7 @@
 class Item < ApplicationRecord
     has_many :join_table_items_carts, dependent: :nullify
+    
+    has_many :join_table_item_orders
     has_many :orders, through: :join_table_item_orders
 
 end
