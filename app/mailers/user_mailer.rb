4373f488a1f3edcf83a6a5ca20ca16a538a18a24
eva.laurent.78@gmail.com <eva@mbp-de-eva.home>
2022-06-09 17:34:58 +0200

    mailjet email send w/ success

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,11 +1,11 @@
 class UserMailer < ApplicationMailer
-    default from: 'no-reply@monsite.fr'
+    default from: 'eva.laurent.78@gmail.com'
 
 
     def order_confirmation(order)
             @order = order
             @user = order.user
-            @url = 'https://hello.com'
+            @url = 'monsite.fr'
             mail(to: @user.email, subject: 'merci pour ta commande bro')
           
     end
